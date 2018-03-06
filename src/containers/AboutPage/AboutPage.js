8820c87dd21533d67fe93df44b6dbd0862d5aadc
Fraser Le Ber <fraser@revlo.co>
2018-03-06 15:09:12 -0800

    improve link styling/format on the about page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -23,35 +23,33 @@ const AboutPage = () => (
     <div styleName="pursuitInfo">
       <h3 className="bold"> Pursuit Client</h3>
       <h5 styleName="linkSpacing"> v{appVersion}</h5>
-      <h5 className="largeText"> The Pursuit client is open source
-      and available on <a
-        styleName="underlinedLink"
-        onClick={(e) => {
-          e.preventDefault();
-          electron.shell.openExternal('https://github.com/pursuit-gg/pursuit-client');
-        }}
-      >Github </a>
+      <h5 className="largeText">
+        The Pursuit client is open source and available on <a
+          styleName="underlinedLink"
+          onClick={(e) => {
+            e.preventDefault();
+            electron.shell.openExternal('https://github.com/pursuit-gg/pursuit-client');
+          }}
+        >Github</a>.
       </h5>
       <h5 className="bold" styleName="acknowledgements"> Acknowledgements </h5>
       <h5>
-        The Pursuit client is built on top of
-        <a
+        The Pursuit client is built on top of <a
           styleName="underlinedLink"
           onClick={(e) => {
             e.preventDefault();
             electron.shell.openExternal('https://github.com/jp9000/obs-studio');
           }}
           target="_blank"
-        > OBS Studio</a> and the
-        <a
+        >OBS Studio</a> and the <a
           styleName="underlinedLink"
           onClick={(e) => {
             e.preventDefault();
             electron.shell.openExternal('https://github.com/stream-labs/obs-studio-node');
           }}
           target="_blank"
-        > OBS Studio Node Wrapper</a>.
-          The hard work done by the OBS and Streamlabs teams makes this project possible.
+        >OBS Studio Node Wrapper</a>.
+        The hard work done by the OBS and Streamlabs teams makes this project possible.
       </h5>
       <div styleName="otherLinks">
         <h5 styleName="linkSpacing"><a
