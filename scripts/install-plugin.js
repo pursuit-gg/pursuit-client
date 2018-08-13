365a0c3f57ae135c93090b42b08e17a68b079848
Fraser Le Ber <fraser@revlo.co>
2018-08-13 14:40:47 -0700

    bump version number for release, update output plugin

--- scripts/install-plugin.js
+++ scripts/install-plugin.js
@@ -6,7 +6,7 @@ const os = require('os');
 const colors = require('colors/safe');
 const zip = require('7zip-bin');
 
-const OBS_FRAME_OUTPUT_VERSION = '0.1.2';
+const OBS_FRAME_OUTPUT_VERSION = '0.1.3';
 
 async function runScript() {
   const zipExe = zip.path7za;
