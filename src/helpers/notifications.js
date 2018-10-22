5f3c79c80934d4732d20215172dd7e6ea1bc7485
Fraser Le Ber <fraser@revlo.co>
2018-10-22 11:39:16 -0700

    improve new match notification logic to handle new modes correctly

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -1,8 +1,11 @@
 export const newMatchCheck = (currentMatchNotifications, nextMatchNotifications) => (
   Object.keys(currentMatchNotifications).length !== 0 &&
-  Object.keys(nextMatchNotifications).reduce((difference, mode) => (
-    difference || (new Date(nextMatchNotifications[mode].latest_match.finished_at) > new Date(currentMatchNotifications[mode].latest_match.finished_at))
-  ), false)
+  Object.keys(nextMatchNotifications).reduce((difference, mode) => {
+    if (!currentMatchNotifications[mode] && nextMatchNotifications[mode].latest_match.finished_at) {
+      return true;
+    }
+    return difference || (new Date(nextMatchNotifications[mode].latest_match.finished_at) > new Date(currentMatchNotifications[mode].latest_match.finished_at));
+  }, false)
 );
 
 export const numNewMatches = matchNotifications => (
