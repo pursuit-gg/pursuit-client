eb32ebd4b551d0fcceea75ec283c6af3eb1da5cd
Fraser Le Ber <fraser@revlo.co>
2018-11-12 11:31:28 -0800

    fix bug with notifications logic

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -1,8 +1,11 @@
 export const newMatchCheck = (currentMatchNotifications, nextMatchNotifications) => (
   Object.keys(currentMatchNotifications).length !== 0 &&
   Object.keys(nextMatchNotifications).reduce((difference, mode) => {
-    if (!currentMatchNotifications[mode] && nextMatchNotifications[mode].latest_match.finished_at) {
-      return true;
+    if (!currentMatchNotifications[mode]) {
+      if (nextMatchNotifications[mode].latest_match.finished_at) {
+        return true;
+      }
+      return difference;
     }
     return difference || (new Date(nextMatchNotifications[mode].latest_match.finished_at) > new Date(currentMatchNotifications[mode].latest_match.finished_at));
   }, false)
